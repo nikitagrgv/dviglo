6929423b8cda86c42d9a3144f54dc3c5169a51fd
Nikita Grigorev <pettyaelse@gmail.com>
2023-07-01 03:09:58 +0700

    render cubes

--- source/vox/main.cpp
+++ source/vox/main.cpp
@@ -21,125 +21,7 @@
 // #include <dviglo/math/random.h>
 #include <dviglo/dviglo_all.h>
 
-#include "PerlinNoise.h"
-
 using namespace dviglo;
-
-class Paintable
-{
-public:
-    virtual ~Paintable() = default;
-    virtual int getWidth() const = 0;
-    virtual int getHeight() const = 0;
-    virtual void set(int x, int y, const Color& color) = 0;
-};
-
-class Painter
-{
-public:
-    virtual ~Painter() = default;
-    Painter(std::unique_ptr<Paintable> paintable)
-        : paintable_(std::move(paintable))
-    {
-    }
-
-    int getWidth() const
-    {
-        assert(paintable_);
-        return paintable_->getWidth();
-    }
-
-    int getHeight() const
-    {
-        assert(paintable_);
-        return paintable_->getHeight();
-    }
-
-    void set(int x, int y, const Color& color)
-    {
-        assert(paintable_);
-        paintable_->set(x, y, color);
-    }
-
-    virtual void draw() = 0;
-
-private:
-    std::unique_ptr<Paintable> paintable_;
-};
-
-class ImagePainter : public Painter
-{
-public:
-    ImagePainter(std::unique_ptr<Paintable> paintable)
-        : Painter(std::move(paintable))
-    {
-    }
-
-    void draw() override
-    {
-        const int w = getWidth();
-        const int h = getHeight();
-
-        for (int i = 0; i < h; i++)
-        {
-            for (int j = 0; j < w; j++)
-            {
-                const float x = static_cast<float>(j) / 30 - offset.x * 100;
-                const float y = static_cast<float>(i) / 30 - offset.y * 100;
-                const Vector2 xy{x, y};
-                const auto col = [this, i, j](float r, float g, float b) { set(j, i, {r, g, b}); };
-
-                float r{};
-                float g{};
-                float b{};
-
-                float intensity = PerlinNoise::getNoise(xy);
-                if (intensity > 0)
-                {
-                    r = intensity;
-                }
-                else
-                {
-                    b = -intensity;
-                }
-                col(r, 0, b);
-
-//                float intensity = (gridGradient(xy, 10) + 1) * 0.5f;
-//                col(intensity, intensity, intensity);
-            }
-        }
-
-        for (int i = 0; i < Min(4, h); i++)
-        {
-            for (int j = 0; j < w; j++)
-            {
-                const float r = (j % 2 == 0) ? 0 : 1;
-                set(j, i, {r, r, r});
-            }
-        }
-    }
-
-    Vector2 offset{0, 0};
-};
-
-class ImageSprite : public Sprite
-{
-public:
-    void setImage(const SharedPtr<Image>& image)
-    {
-        auto texture = new Texture2D();
-        texture->SetFilterMode(FILTER_NEAREST);
-        texture->SetData(image);
-        SetTexture(texture);
-        SetFixedWidth(static_cast<int>(static_cast<float>(image->GetWidth()) * scale_));
-        SetFixedHeight(static_cast<int>(static_cast<float>(image->GetHeight()) * scale_));
-    }
-
-    void setScale(float scale) { scale_ = scale; }
-
-private:
-    float scale_ = 1;
-};
 
 class App : public Application
 {
@@ -154,171 +36,108 @@ public:
     {
         DV_INPUT->SetMouseVisible(true);
         subscribe_to_event(E_UPDATE, DV_HANDLER(App, on_update));
-        subscribe_to_event(E_KEYDOWN, DV_HANDLER(App, on_key_down));
 
-        init_gui();
         init_world();
     }
 
 private:
-    void init_gui()
-    {
-        auto* style = DV_RES_CACHE->GetResource<XmlFile>("ui/default_style.xml");
-        DV_UI->GetRoot()->SetDefaultStyle(style);
-
-        auto* scroll = new ListView();
-        DV_UI->GetRoot()->AddChild(scroll);
-        scroll->SetStyleAuto();
-        scroll->SetMinWidth(800);
-        scroll->SetMinHeight(800);
-
-        auto* container = new UiElement();
-        container->SetLayout(LM_VERTICAL, 5, {3, 3, 3, 3});
-        container->SetStyleAuto();
-        scroll->SetContentElement(container);
-
-        auto* texture = new Texture2D();
-        texture->SetSize(220, 220, Graphics::GetRGBFormat(), TEXTURE_DYNAMIC, 0);
-
-        image_ = texture->GetImage();
-        for (int i = 0; i < image_->GetWidth(); i++)
-        {
-            for (int j = 0; j < image_->GetHeight(); j++)
-            {
-                image_->SetPixel(i, j, Color::GRAY);
-            }
-        }
-
-        class PaintableImage : public Paintable
-        {
-        public:
-            PaintableImage(WeakPtr<Image> image)
-                : image_(std::move(image))
-            {
-            }
-
-            int getWidth() const override { return image_->GetWidth(); }
-            int getHeight() const override { return image_->GetHeight(); }
-            void set(int x, int y, const dviglo::Color& color) override
-            {
-                image_->SetPixel(x, y, color);
-            }
-
-        private:
-            WeakPtr<Image> image_;
-        };
-        std::unique_ptr<Paintable> paintable = std::make_unique<PaintableImage>(image_);
-
-        painter_ = std::make_unique<ImagePainter>(std::move(paintable));
-
-        sprite_ = new ImageSprite();
-        container->AddChild(sprite_);
-        sprite_->SetStyleAuto();
-        sprite_->setImage(image_);
-        sprite_->setScale(3);
-
-        {
-            slider_x_ = new Slider();
-            container->AddChild(slider_x_);
-            slider_x_->SetStyleAuto();
-            slider_x_->SetRange(2);
-            slider_x_->SetValue(1);
-            slider_x_->SetMinHeight(20);
-        }
-        {
-            slider_y_ = new Slider();
-            container->AddChild(slider_y_);
-            slider_y_->SetStyleAuto();
-            slider_y_->SetRange(2);
-            slider_y_->SetValue(1);
-            slider_y_->SetMinHeight(20);
-        }
-        checkbox_ = new CheckBox();
-        container->AddChild(checkbox_);
-        checkbox_->SetChecked(true);
-        checkbox_->SetStyleAuto();
-        checkbox_->SetFixedWidth(20);
-        checkbox_->SetFixedHeight(20);
-
-        fps_text_ = new Text();
-        container->AddChild(fps_text_);
-        fps_text_->SetStyleAuto();
-        fps_text_->SetFontSize(20);
-        fps_text_->SetMinHeight(20);
-        fps_text_->SetMinWidth(300);
-        fps_text_->SetText("fasfaf");
-    }
-
     void init_world()
     {
         auto cache = DV_RES_CACHE;
+        auto renderer = DV_RENDERER;
 
-        scene_ = new Scene();
-        scene_->create_component<Octree>();
+        auto scene = new Scene();
+        scene->create_component<Octree>();
 
-        auto light_node = new Node();
-        scene_->AddChild(light_node);
-        auto light = new Light();
-        light_node->AddComponent(light, 0, REPLICATED);
+        camera_node_ = scene->create_child();
+        auto camera = camera_node_->create_component<Camera>();
+        camera_node_->SetPosition({-0.1f, 0.2f, -5.f});
 
-        camera_node_ = new Node();
-        scene_->AddChild(camera_node_);
-        auto camera = new Camera();
-        camera_node_->AddComponent(camera, 0, REPLICATED);
+        auto viewport = new Viewport(scene, camera);
+        renderer->SetViewport(0, viewport);
 
-        auto box_node = new Node();
-        scene_->AddChild(box_node);
-        auto model = new StaticModel();
-        box_node->AddComponent(model, 0, REPLICATED);
-        model->SetModel(cache->GetResource<Model>("models/box.mdl"));
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                for (int k = 0; k < 10; k++)
+                {
+                    auto box_node = scene->create_child();
+                    box_node->SetPosition({i * 2.f, j * 2.f, k * 2.f});
+                    auto model = box_node->create_component<StaticModel>();
+                    model->SetModel(cache->GetResource<Model>("models/box.mdl"));
+                    cubes_.Push(WeakPtr(box_node));
+                }
+            }
+        }
 
-        SharedPtr<Viewport> viewport(new Viewport(scene_, camera));
-        DV_RENDERER->SetViewport(0, viewport);
+        auto light_node = scene->create_child();
+        auto light = light_node->create_component<Light>();
+        light->SetLightType(LIGHT_POINT);
+        light_node->SetPosition({1, 1, 1});
+        light->SetColor(Color::GREEN);
     }
 
     void on_update(StringHash /*event*/, VariantMap& data)
     {
         const float dt = data[Update::P_TIMESTEP].GetFloat();
-        fps_text_->SetText(String(1/dt));
-        sprite_->setImage(image_);
-        update_image();
-    }
+        auto input = DV_INPUT;
+        const float speed = 3.f;
+        const float rot_speed = 15.0f;
 
-    void on_key_down(StringHash /*event*/, VariantMap& data)
-    {
-        const i32 key = data[KeyDown::P_KEY].GetI32();
-        if (key == KEY_ESCAPE)
+        Vector3 dir;
+        if (input->GetKeyDown(KEY_D))
         {
-            DV_ENGINE->Exit();
+            dir.x += speed;
         }
-        if (key == KEY_SPACE)
+        if (input->GetKeyDown(KEY_A))
         {
+            dir.x -= speed;
         }
-    }
+        if (input->GetKeyDown(KEY_W))
+        {
+            dir.z += speed;
+        }
+        if (input->GetKeyDown(KEY_S))
+        {
+            dir.z -= speed;
+        }
+        if (input->GetKeyDown(KEY_E))
+        {
+            dir.y += speed;
+        }
+        if (input->GetKeyDown(KEY_Q))
+        {
+            dir.y -= speed;
+        }
+        const Matrix3x4 rot_matr(Vector3{0, 0, 0}, camera_node_->GetWorldRotation(), 1.f);
+        dir = rot_matr * dir;
 
-    void update_image()
-    {
-        if (!checkbox_->IsChecked())
+        float rot = 0;
+        if (input->GetKeyDown(KEY_LEFT))
         {
-            return;
+            rot -= rot_speed;
         }
-        painter_->offset.x = slider_x_->GetValue() - slider_x_->GetRange() / 2;
-        painter_->offset.y = slider_y_->GetValue() - slider_y_->GetRange() / 2;
-        painter_->draw();
+        if (input->GetKeyDown(KEY_RIGHT))
+        {
+            rot += rot_speed;
+        }
+
+        camera_node_->RotateAround({0, 0}, Quaternion(rot * dt, {0,1,0}), dviglo::TransformSpace::Local);
+        camera_node_->SetPosition(camera_node_->GetPosition() + dir * dt);
+
+        // random rotate
+        for (const auto& cube : cubes_)
+        {
+            cube->RotateAround(
+                Vector3{}, Quaternion(dt * 5.f, Vector3{Random(1.0f), Random(1.0f), Random(1.0f)}),
+                dviglo::TransformSpace::Local);
+        }
     }
 
 private:
-    SharedPtr<Scene> scene_;
-    SharedPtr<Node> camera_node_;
-
-    std::unique_ptr<ImagePainter> painter_;
-
-    SharedPtr<Image> image_;
-    WeakPtr<Text> fps_text_;
-    WeakPtr<CheckBox> checkbox_;
-    WeakPtr<ImageSprite> sprite_;
-    WeakPtr<Slider> slider_x_;
-    WeakPtr<Slider> slider_y_;
+    Vector<WeakPtr<Node>> cubes_;
+    WeakPtr<Node> camera_node_;
 };
 
 int main(int argc, char** argv)
